f9ca00cb5c9f1f73b126608aa792cad59ec59b14
Asilbek Dev <asilbekegamnazarov999@gmail.com>
2025-09-04 15:41:48 +0500

    zero from

--- i18n/routing.ts
+++
@@ -1,7 +0,0 @@
-import { defineRouting } from "next-intl/routing";
-
-export const routing = defineRouting({
-    locales: ['uz', 'rz', 'en'],
-    defaultLocale: 'uz',
-    localePrefix: 'as-needed'
-})
--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://uzum-market-online.vercel.app',
-    generateRobotsTxt: true,
-    sitemapSize: 7000,
-    outDir: './public',
-};
