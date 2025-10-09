b0dea5468cf08eab6799dbcfd19c01e7d7a6a492
Asilbek Dev <asilbekegamnazarov999@gmail.com>
2025-10-09 16:02:24 +0500

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,56 @@
-import Image from "next/image";
+import React from 'react'
+import { Metadata } from 'next'
+import Hero from '@/components/layout/Hero'
 
-export default function Home() {
+export const metadata: Metadata = {
+  title: "Uzum Market — O‘zbekistondagi Internet-do'kon va Marketpleys",
+  description: "Mahsulotlar 90% gacha chegirmada🔥 12 oylik muddatli to'lov. 1 kunda yetkazib berish. Elektronika, maishiy texnika, bolalar buyumlari, kiyim-kechak va poyabzal, uy, sport, salomatlik va go'zallik uchun zarur hamma narsa, kitoblar, zoomahsulotlar va boshqa ko‘plab narsalar. Chegirma va aksiyalar har kuni Uzum Marketda",
+  keywords: "uzum, clone, uzbekistan, e-commerce, online shopping, online store, savdo, mahsulotlar, chegirmalar, yangi kelganlar",
+  authors: [{ name: 'Asilbek', url: 'https://t.me/personal_telegram_account_2' }],
+  creator: 'Asilbek',
+  publisher: 'Uzum Market clone',
+  openGraph: {
+    title: "Uzum Market — O‘zbekistondagi Internet-do'kon va Marketpleys",
+    description: "Mahsulotlar 90% gacha chegirmada🔥 12 oylik muddatli to'lov. 1 kunda yetkazib berish. Elektronika, maishiy texnika, bolalar buyumlari, kiyim-kechak va poyabzal, uy, sport, salomatlik va go'zallik uchun zarur hamma narsa, kitoblar, zoomahsulotlar va boshqa ko‘plab narsalar. Chegirma va aksiyalar har kuni Uzum Marketda",
+    url: 'https://www-uzum-uz.vercel.app/uz',
+    siteName: 'uzum market clone',
+    images: [
+      {
+        url: 'https://example.com/og-image.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Open Graph rasmi tavsifi',
+      },
+    ],
+    locale: 'uz_UZ',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "Uzum Market — O‘zbekistondagi Internet-do'kon va Marketpleys",
+    description: "Mahsulotlar 90% gacha chegirmada🔥 12 oylik muddatli to'lov. 1 kunda yetkazib berish. Elektronika, maishiy texnika, bolalar buyumlari, kiyim-kechak va poyabzal, uy, sport, salomatlik va go'zallik uchun zarur hamma narsa, kitoblar, zoomahsulotlar va boshqa ko‘plab narsalar. Chegirma va aksiyalar har kuni Uzum Marketda",
+    images: ['https://example.com/twitter-image.jpg'],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+  },
+  alternates: {
+    canonical: 'https://www-uzum-uz.vercel.app/uz',
+    languages: {
+      'uz-UZ': 'https://www-uzum-uz.vercel.app/uz',
+      'ru-RU': 'https://www-uzum-uz.vercel.app/ru',
+    },
+  },
+};
+
+const Home = () => {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    <>
+      <Hero />
+    </>
+  )
 }
+
+export default Home
---
+++ src/components/layout/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='bg-hero-pattern h-[400px] md:h-[600px] bg-cover bg-center flex items-center justify-center'>
+fgnm
+    </section>
+  )
+}
+
+export default Hero
