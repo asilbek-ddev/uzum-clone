6e6b397aaba840dce9ab34d5b85031ed642775c8
Asilbek Egamnazarov <asilbekegamnazarov999@gmail.com>
2026-01-30 22:19:42 -0500

    Add global styles, layout, and home page component
    - Created a new globals.css file for global styles including dark mode support.
    - Added layout.tsx for the root layout with metadata and font integration.
    - Implemented a simple home page component in page.tsx.
    - Declared CSS module support in global.d.ts.

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const InterFont = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Uzum Clone – Portfel loyiham",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${InterFont.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
